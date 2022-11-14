0958a636eb2eff942a119cb122e9a45a6a6d9f9c
tugba-koc <koctug@itu.edu.tr>
2022-11-14 14:19:48 +0300

    feat : add dispatch action type

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -1,14 +1,14 @@
 import { Dispatch } from 'react';
-import { ActionType, IProductItem } from '../types/reducer';
+import { ActionType, IProductItem, SetFilterProductAction, SetProductAction, SetProductDetailAction } from '../types/reducer';
 
-const renderProduct = (value: IProductItem[], dispatch: Dispatch<any>) => {
+const renderProduct = (value: IProductItem[], dispatch: Dispatch<SetProductAction>) => {
   dispatch({
     type: ActionType.SET_PRODUCT,
     payload: value,
   });
 };
 
-const renderProductDetail = (value: IProductItem, dispatch: Dispatch<any>) => {
+const renderProductDetail = (value: IProductItem, dispatch: Dispatch<SetProductDetailAction>) => {
   dispatch({
     type: ActionType.SET_PRODUCT_DETAIL,
     payload: value,
@@ -17,7 +17,7 @@ const renderProductDetail = (value: IProductItem, dispatch: Dispatch<any>) => {
 
 const renderFilteredProduct = (
   value: IProductItem[],
-  dispatch: Dispatch<any>
+  dispatch: Dispatch<SetFilterProductAction>
 ) => {
   dispatch({
     type: ActionType.FILTER_PRODUCT,
